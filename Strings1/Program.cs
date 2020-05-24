95b9e27c4c228ec9c7c4a969dc568e93eaded7a6
Artur Siewniak <43797135+ArturSiewniak@users.noreply.github.com>
2020-05-24 20:55:16 +0200

    Javatpoint - Strings

---
+++ Strings1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Sup";      //string keyword
+            String s2 = "Waddup";   //String class
+
+            string s3 = "Hello";
+            char[] ch = { 'C', 's', 'h', 'a', 'r', 'p' };
+            string s4 = new string(ch);
+
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+
+            //Refer to the website or other sources for individual methods of string
+        }
+    }
+}
