29befb7566256d436259a573f245e63bb1a842b4
Artur Siewniak <43797135+ArturSiewniak@users.noreply.github.com>
2020-05-26 23:19:06 +0200

    Javatpoint - Delegates

---
+++ Delegates1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate int Calculator(int n);
+
+namespace Delegates1
+{
+    class Program
+    {
+        static int number = 100;
+        public static int add(int n)
+        {
+            number = number + n;
+            return number;
+        }
+
+        public static int mul(int n)
+        {
+            number = number * n;
+            return number;
+        }
+
+        public static int getNumber()
+        {
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator(add);
+            Calculator c2 = new Calculator(mul);
+            c1(20);
+            Console.WriteLine("C1 delegate: " + getNumber());
+            c2(2);
+            Console.WriteLine("C2 delegate: " + getNumber());
+        }
+    }
+}
