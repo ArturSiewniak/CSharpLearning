2ff5c8fb5f59582ce7a1fb603915cb4d4b5c18c3
Artur Siewniak <43797135+ArturSiewniak@users.noreply.github.com>
2020-05-26 23:38:40 +0200

    Javatpoint - Anonymous Function

---
+++ AnonymousFunctions1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousFunctions1
+{
+    class Program
+    {
+        delegate int Square(int num);
+        public delegate void AnonymousFun();
+        static void Main(string[] args)
+        {
+            Square GetSquare = x => x * x;
+            int i = GetSquare(5);
+            Console.WriteLine("Square: " + i);
+
+            AnonymousFun fun = delegate ()
+            {
+                Console.WriteLine("This is anonymous function");
+            };
+            fun();
+        }
+    }
+}
