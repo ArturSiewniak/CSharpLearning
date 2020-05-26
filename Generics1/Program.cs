4f9329c8a96891fd0d19bdecb6757a4dea0f20cc
Artur Siewniak <43797135+ArturSiewniak@users.noreply.github.com>
2020-05-26 23:12:55 +0200

    Javatpoint - Generics

---
+++ Generics1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics1
+{
+    class GenericClass<T>
+    {
+        public GenericClass(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        
+    }
+
+    class GenericClass1
+    {
+        public void Show<T>(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> genstring = new GenericClass<string>("This is a generic class");
+            GenericClass<int> genInt = new GenericClass<int>(123);
+            GenericClass<char> genChar = new GenericClass<char>('a');
+
+            GenericClass1 genC = new GenericClass1();
+            genC.Show("This is a string");
+            genC.Show(321);
+            genC.Show('A');
+        }
+    }
+}
